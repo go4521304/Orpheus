7e36858b8843fa25f19ad34297b73c0e182ee127
Kclient <go4521304@gmail.com>
2022-09-14 21:16:21 +0900

    Tick Event / Bpm, Tempo Set
    - Need making block
    - When tickevent invoke, player move to the next block

--- Orpheus_Unity/Assets/Bridge2.cs
+++ Orpheus_Unity/Assets/Bridge2.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bridge2 : MonoBehaviour
 {
+    GameManager gameManager;
+
     [SerializeField] GameObject bridge;
-
     [SerializeField] float length;
-
     [SerializeField][Range(1f, 20f)] float speed;
 
     public List<GameObject> bridges = new List<GameObject>();
 
     float width = 36.754f;
 
+    private KEY key = KEY.None;
+
+    private bool isUpdate = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameManager.Instance;
+        gameManager.tickEvent.AddListener(TickEvt);
+
         GameObject obj;
-        Vector3 pos = new Vector3(10.146f, -2.25f, 0);
+        Vector3 pos = new Vector3(10.615f, -2.211f, 0);
 
         while (bridges.Count * width < length)
         {
@@ -29,11 +37,33 @@ public class Bridge2 : MonoBehaviour
 
             bridges.Add(obj);
         }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.left * Time.time * speed;
+        if (key == KEY.Up)
+        {
+
+        }
+        else if (key == KEY.Down)
+        {
+
+        }
+        else if (key == KEY.Left)
+        {
+
+        }
+        else if (key == KEY.Right)
+        {
+
+        }
+        //transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    void TickEvt(int mKey)
+    {
+        key = (KEY)mKey;
     }
 }
--- Orpheus_Unity/Assets/Script/GameManager.cs
+++ Orpheus_Unity/Assets/Script/GameManager.cs
@@ -1,25 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Device;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 using Screen = UnityEngine.Device.Screen;
+
+enum KEY { None, Up, Down, Left, Right, Limit };
+
+[System.Serializable] public class TickEvent : UnityEvent<int> { }
 
 public class GameManager : MonoBehaviour
 {
-    private enum KEY {None, Up, Down, Left, Right};
+    private static GameManager gameManager = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (gameManager != null)
+                return gameManager;
+            else
+                return null;
+        }
+    }
+
+    public TickEvent tickEvent;
+
     private KEY key = KEY.None;
+
+    [SerializeField] private int Bpm = 60;
+    [SerializeField] private double Tempo = 1;
+
+    private double timeGap;
+
     private double time = 1;
 
 
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, true);
+
+        if (gameManager == null)
+        {
+            gameManager = this;
+
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        tickEvent = new TickEvent();
+
+        timeGap = (Bpm / 60) * Tempo;
+        time = timeGap;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -27,11 +71,13 @@ public class GameManager : MonoBehaviour
     {
         if (time < 0)
         {
+            tickEvent.Invoke((int)key);
+
             // Processing
             if (key != KEY.None)
                 Debug.Log(key);
 
-            time = 1;
+            time = timeGap;
             key = KEY.None;
         }
         else
